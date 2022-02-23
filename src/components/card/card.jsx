d84f618790ef8c44e1da9af66b691457d9f0984d
G7M7T1 <genlisense@gmail.com>
2022-02-23 07:17:15 -0500

    Update

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { gsap } from "gsap";
 import "./card.scss"
 
-export const Card = (props) => (
-    <div className="card-container">
-        <img src={`https://robohash.org/${props.monster.id}?set=set2`} alt="Monster"/>
-        <h1>{props.monster.name}</h1>
-    </div>
-)
+export const Card = (props) => {
+    useEffect(() => {
+        gsap.fromTo(".card-container",
+            {scale:1.2, autoAlpha: 0},
+            {scale:1, stagger: 0.2, autoAlpha: 1})
+    });
+
+    function mouseIn(event) {
+        gsap.to(event.target, {scale: 1.05, duration: 0.1})
+    }
+
+    function mouseOut(event) {
+        gsap.to(event.target, {scale: 1, duration: 0.1})
+    }
+
+    return(
+        <div className="card-container" onMouseEnter={mouseIn} onMouseLeave={mouseOut} >
+            <img src={`https://robohash.org/${props.monster.id}?set=set2`} alt="Monster"/>
+            <h1>{props.monster.name}</h1>
+        </div>
+    )
+}
