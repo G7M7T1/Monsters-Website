b13736f7937757b31d2d88902f0210465099248c
G7M7T1 <genlisense@gmail.com>
2022-02-23 06:02:24 -0500

    Add Photo

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,6 +3,7 @@ import "./card.scss"
 
 export const Card = (props) => (
     <div className="card-container">
+        <img src={`https://robohash.org/${props.monster.id}?set=set2`} alt="Monster"/>
         <h1>{props.monster.name}</h1>
     </div>
 )
